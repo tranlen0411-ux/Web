08a2a181170b1d648901601576a4ee90f58b839f
tranlen0411-ux <tranlen0411@gmail.com>
2026-09-01 21:42:31 +0700

    Fix Question Bank duplicate import compile errors

--- src/components/dashboard/question-bank/ImportQuestionBankModal.jsx
+++ src/components/dashboard/question-bank/ImportQuestionBankModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import {
   X,
@@ -27,6 +27,7 @@ import {
 import {
   downloadQuestionBankExcelTemplate,
   downloadQuestionBankWordTemplate
+} from '../../../utils/questionBankTemplateGenerators';
 import {
   toQuestionBankPayload,
   findDuplicatesInQuestionList,
